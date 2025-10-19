a2b3e456d89516f9ff17b9f345d40c3e58ed60fd
AyoubHmadouch <ayoubhmd@hotmail.fr>
2025-10-19 13:41:00 +0200

    feat: add terraform pipeline

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@
 module "eks-vpc" {
   source = "git::https://github.com/AyoubHmadouch/terraform-aws-vpc.git?ref=master"
 
-  name     = var.vpc_prefix
-  vpc_cidr = var.cidr_block
-  tier     = 2
-  az_count = 2
+  name                      = var.vpc_prefix
+  vpc_cidr                  = var.cidr_block
+  tier                      = 2
+  az_count                  = 2
   enable_nat_gateway_per_az = false # deploy only one nat gateway
   public_subnet_tags = {
     "kubernetes.io/role/elb" = "1"
